dbf0edaf88e1efd01e0641d4f32eb14e9f9626f7
Prem Sundar Chakravarty <psc@PSC.rjpvmware.com>
2020-10-03 21:18:57 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/finallogin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.finallogin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/authentication.dart
@@ -0,0 +1,56 @@
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+import 'http_exception.dart';
+
+class Authentication with ChangeNotifier
+{
+
+  Future<void> signUp(String email, String password) async
+  {
+    const url= 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCiEU9Xovr5-l4NSd0RDMkEPLkPncHgj1o';
+    try{
+      final response = await http.post(url, body: json.encode(
+          {
+            'email' : email,
+            'password' : password,
+            'returnSecureToken' : true,
+          }
+      ));
+      final responseData = json.decode(response.body);
+//      print(responseData);
+      if(responseData['error'] != null)
+      {
+        throw HttpException(responseData['error']['message']);
+      }
+
+
+    } catch (error)
+    {
+      throw error;
+    }
+
+  }
+  Future<void> logIn(String email, String password) async
+  {
+    const url= 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCiEU9Xovr5-l4NSd0RDMkEPLkPncHgj1o';
+    try{
+      final response = await http.post(url, body: json.encode(
+          {
+            'email' : email,
+            'password' : password,
+            'returnSecureToken' : true,
+          }
+      ));
+      final responseData = json.decode(response.body);
+      if(responseData['error'] != null)
+      {
+        throw HttpException(responseData['error']['message']);
+      }
+//      print(responseData);
+    } catch(error)
+    {
+      throw error;
+    }
+  }
+}
